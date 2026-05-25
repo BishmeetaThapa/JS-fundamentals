707eec87a56fe33e4bcb561b9beba887c7fab333
Bishmeeta <jhalakkumarithapa520@gmail.com>
2026-05-25 18:39:33 +0545

    Create counter app using React useState hook

---
+++ counter-app/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Counter App</h1>
+
+      <h2>{count}</h2>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+
+      <button
+        onClick={() => setCount(count - 1)}
+        style={{ marginLeft: "10px" }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+}
+
+export default App;
